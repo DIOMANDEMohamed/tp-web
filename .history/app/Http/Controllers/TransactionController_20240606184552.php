c301b7061bb6ed53927669969e98d14d39c0d281
DIOMANDEMohamed <diomandemohamed661@gmail.com>
2024-06-06 22:06:29 +0200

    mise à jour

---
+++ .history/app/Http/Controllers/TransactionController_20240606184552.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    //
+    public function transaction(){
+        return view('all transaction.index');
+    }
+
+    public function trandepot(){
+        return view('depot.index');
+
+    }
+    public function trancompleter(){
+        return view('completer.index');
+    }
+    public function tranenattente(){
+        return view('en attente.index');
+    }
+    public function tranretirer(){
+        return view('retirer.index');
+    }
+    public function tranonhold(){
+        return view('onhold.index');
+    }
+    public funtion tranformulaire(){
+        return view('transaction.formulaire');
+    }
+}
+
---
+++ .history/app/Models/transaction_20240606210145.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class transaction extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'commande',
+        'utilisateur',
+        'detail',
+        'par',
+        'montant',
+        'status',
+    ];
+}

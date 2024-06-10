03e8510ab977fabaa1c8ec5a5f0ef80f4de7fa0b
DIOMANDEMohamed <diomandemohamed661@gmail.com>
2024-06-10 19:25:43 +0200

    mise à jour

---
+++ .history/resources/views/transactions/index.blade_20240610191437.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des Transactions</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h2>Liste des Transactions</h2>
+        <hr>
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Commande</th>
+                    <th>Utilisateur</th>
+                    <th>Detail</th>
+                    <th>par</th>
+                    <th>Montant</th>
+                    <th>Status</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($transactions as $transaction)
+                    <tr>
+                        <td>{{ $transaction->id }}</td>
+                        <td>{{ $transaction->commande }}</td>
+                        <td>{{ $transaction->utilisateur }}</td>
+                        <td>{{ $transaction->detail }}</td>
+                        <td>{{ $transaction->par }}</td>
+                        <td>{{ $transaction->montant }}</td>
+                        <td>{{ $transaction->status }}</td>
+                        <td>
+                            <a href="#" class="btn btn-primary">Voir</a>
+                            <a href="#" class="btn btn-secondary">Éditer</a>
+                            <form action="#" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <a href="{{ route('transaction.delete'), }}" class="btn btn-secondary">delete</a>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
